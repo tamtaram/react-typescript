6c948f45e6fe57f09f95478f61da8fa5bbc4b9a6
Lotten Wester <lotten.wester@appliedtechnology.se>
2023-01-05 16:16:18 +0100

    Change name

--- src/App.tsx
+++ src/App.tsx
@@ -40,6 +40,9 @@ function App() {
   const [userName, setUserName] = useState<IName>();
   const [userAddress, setUserAddress] = useState<IAdress>();
   const [userDob, setUserDob] = useState<IDob>();
+  const [newFirstName,setNewFirstName] = useState<string>("");
+  const [newLastName,setNewLastName] = useState<string>("");
+
 
 
   useEffect(() => {
@@ -47,7 +50,6 @@ function App() {
       const response = await fetch("https://randomuser.me/api/");
       const results = await response.json();
       setUserData(results);
-     
     }
     getData();
   }, []);
@@ -55,23 +57,39 @@ function App() {
   useEffect(() => {
     setUserName(userData?.results[0].name)
     setUserAddress(userData?.results[0].location)
-    setUserDob(userData?.results[0].dob)
+    setUserDob(userData?.results[0].dob)}
+  , [userData])
 
-  }, [userData])
+  const getName = () => `${userName?.first} ${userName?.last}`;
+  const getAddress = () => `${userAddress?.street.name} ${userAddress?.street.number} 
+   ${userAddress?.postcode} ${userAddress?.city}
+  ${userAddress?.state}
+  ${userAddress?.country}` 
 
-  /*
+  const handleSubmit = () => {
+    setUserName({first: newFirstName ? newFirstName : "", last: newLastName ? newLastName : ""});
+  }
 
-  */
-  
   return (
     <div className="App">
       <ul>
-        <li>{userData?.results[0].gender}</li> 
-        <li>{userName?.first}</li>
-
-<li>{userAddress?.country} </li>
-<li>{userDob?.age}</li> 
+        <li>Name: {getName()}</li>
+        <li>Address: {getAddress()} </li>
+        <li>Age: {userDob?.age}</li> 
+        <li>Gender: {userData?.results[0].gender}</li> 
      </ul>
+     <form onSubmit={handleSubmit}>
+      <label>
+        First name:
+      <input type="text" id="firstname" name="user_name" value={newFirstName}
+          onChange={(e) => setNewFirstName(e.target.value)}
+ /></label><br />
+ <label>Last name: 
+ <input type="text" id="lastname" name="user_name" value={newLastName}
+          onChange={(e) => setNewLastName(e.target.value)}
+ /></label><br />
+      <button type="submit">Change name</button>
+     </form>
     </div>
   );
 }
