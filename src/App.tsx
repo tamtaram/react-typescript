a70d4bdd7812982e49ca96b8d005fc14d167e59c
Lotten Wester <lotten.wester@appliedtechnology.se>
2023-01-05 15:02:46 +0100

    Fetch data from api

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect} from 'react';
 import './App.css';
 
+interface IName {
+  first: string,
+  last: string,
+}
+
+interface IAdress {
+  city: string,
+  state: string,
+  country: string,
+  postcode: number,
+  street: IStreet
+}
+
+interface IStreet {
+  number: number,
+  name: string
+}
+
+interface IDob {
+  age: number
+}
+
+interface IUserInfo {
+  gender: string,
+  email: string,
+  name: IName,
+  location: IAdress,
+  dob: IDob
+}
+
+interface IResults {
+  results: IUserInfo[]
+}
+
 function App() {
+  const [userData, setUserData] = useState<IResults>();
+  const [userName, setUserName] = useState<IName>();
+  const [userAddress, setUserAddress] = useState<IAdress>();
+  const [userDob, setUserDob] = useState<IDob>();
+
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetch("https://randomuser.me/api/");
+      const results = await response.json();
+      setUserData(results);
+     
+    }
+    getData();
+  }, []);
+
+  useEffect(() => {
+    setUserName(userData?.results[0].name)
+    setUserAddress(userData?.results[0].location)
+    setUserDob(userData?.results[0].dob)
+
+  }, [userData])
+
+  /*
+
+  */
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        <li>{userData?.results[0].gender}</li> 
+        <li>{userName?.first}</li>
+
+<li>{userAddress?.country} </li>
+<li>{userDob?.age}</li> 
+     </ul>
     </div>
   );
 }
 
 export default App;
+
